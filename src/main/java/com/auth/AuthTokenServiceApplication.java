b13f696d7f463f8526daf2939e56856411d701e5
Gabriel <gabrieljn2014@gmail.com>
2025-04-26 22:33:24 -0300

    Removido atributos desnecessários do application properties

---
+++ src/main/java/com/auth/AuthTokenServiceApplication.java
@@ -0,0 +1,12 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTokenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTokenServiceApplication.class, args);
+	}
+}
